0727d410afe2d59f750a0511e6f99aeee3201840
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-03-02 11:51:03 +0530

    cart page designed

--- e-commerce/src/User/Component/Cart/Cart.jsx
+++ e-commerce/src/User/Component/Cart/Cart.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
 import CartItem from './CartItem'
+import { Button } from '@mui/material'
 
 const Cart = () => {
     return (
         <div>
-            <div className='lg:grid grid-cols-3'>
-                <CartItem />
+            <div className='lg:grid grid-cols-3 lg:px-16 relative pt-3'>
+                <div className='col-span-2'>
+                    <CartItem />
+                </div>
+
+                <div className='px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0'>
+                    <div className="border">
+                        <p className='p-3'>Price Details</p>
+                        <hr />
+                        <div className='space-y-3 font-semibold p-3'>
+                            <div className='flex justify-between pt-3 text-black'>
+                                <span>Price</span>
+                                <span>₹599</span>
+                            </div>
+                            <div className='flex justify-between pt-3 text-black'>
+                                <span>Delivery</span>
+                                <span className='text-green-600'>Free</span>
+                            </div>
+                            <div className='flex justify-between pt-3 text-black'>
+                                <span>Discount</span>
+                                <span className='text-green-600'>-₹5999</span>
+                            </div>
+                            <hr />
+                            <div className='flex justify-between pt-3 text-black '>
+                                <span className='font-bold'>Total Price</span>
+                                <span>₹5999</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <button
+                        type="submit"
+                        className="mt-2 flex w-full items-center justify-center rounded-md border border-transparent
+                         bg-lime-600 px-8 py-3 text-base text-white
+                          hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 font-semibold"
+                    >
+                        Checkout
+                    </button>
+                </div>
             </div>
         </div>
     )
--- e-commerce/src/User/Component/Product/ProductCard.jsx
+++ e-commerce/src/User/Component/Product/ProductCard.jsx
@@ -269,7 +269,7 @@ export default function Example() {
 
                 <button
                   type="submit"
-                  className="mt-10 flex w-[11rem] items-center justify-center rounded-md border border-transparent bg-lime-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="mt-10 flex w-[11rem] items-center justify-center rounded-md border border-transparent bg-lime-600 px-8 py-3 text-base font-medium text-white hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2"
                 >
                   <AddShoppingCartIcon />
                   Add to cart
