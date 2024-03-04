3acf4678c29123504c822ab7d01d054d7e8dd84d
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-03-04 23:48:37 +0530

    cart redux store

---
+++ e-commerce/src/Redux/Cart/Action.js
@@ -0,0 +1,42 @@
+import { api } from "../../Config/ApiConfig"
+import { ADD_TO_CART_FAILURE, ADD_TO_CART_REQUEST, ADD_TO_CART_SUCCESS, GET_CART_FAILURE, GET_CART_REQUEST, GET_CART_SUCCESS, REMOVE_CART_ITEM_FAILURE, REMOVE_CART_ITEM_REQUEST, REMOVE_CART_ITEM_SUCCESS, UPDATE_CART_ITEM_FAILURE, UPDATE_CART_ITEM_REQUEST, UPDATE_CART_ITEM_SUCCESS } from "./ActionType"
+
+export const addToCart=(reqData)=>async (dispatch)=>{
+    dispatch({type:ADD_TO_CART_REQUEST})
+    try {
+        const {data}=await api.put("/api/cart/add",reqData.data)
+        dispatch({type:ADD_TO_CART_SUCCESS,payload:data})
+    } catch (error) {
+        dispatch({type:ADD_TO_CART_FAILURE,payload:error.message})
+    }
+}
+
+export const usersCart=()=>async (dispatch)=>{
+    dispatch({type:GET_CART_REQUEST})
+    try {
+        const {data}=await api.get("/api/cart/")
+        dispatch({type:GET_CART_SUCCESS,payload:data})
+    } catch (error) {
+        dispatch({type:GET_CART_FAILURE,payload:error.message})
+    }
+}
+
+export const removeCartItem=(reqData)=>async (dispatch)=>{
+    dispatch({type:REMOVE_CART_ITEM_REQUEST})
+    try {
+        const {data}=await api.delete(`/api/cart/${reqData.cartItemId}`)
+        dispatch({type:REMOVE_CART_ITEM_SUCCESS,payload:data})
+    } catch (error) {
+        dispatch({type:REMOVE_CART_ITEM_FAILURE,payload:error.message})
+    }
+}
+
+export const updateCartItem=(reqData)=>async (dispatch)=>{
+    dispatch({type:UPDATE_CART_ITEM_REQUEST})
+    try {
+        const {data}=await api.put(`/api/cart/${reqData.cartItemId}`,reqData.data)
+        dispatch({type:UPDATE_CART_ITEM_SUCCESS,payload:data})
+    } catch (error) {
+        dispatch({type:UPDATE_CART_ITEM_FAILURE,payload:error.message})
+    }
+}
