b6801ae1b4377c469cb3f09abff816e678bfcd73
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-03-11 21:58:20 +0530

    payment gateway integration

--- e-commerce/src/User/Component/HomeCrousel/CrouselData.js
+++ e-commerce/src/User/Component/HomeCrousel/CrouselData.js
@@ -4,7 +4,7 @@ export const CrouselData=[
         
     },
     {
-        image:"https://images-eu.ssl-images-amazon.com/images/G/31/img21/MA2024/NewLaunches/GW/27feb/3000._CB581842874_.jpg"
+        image:"https://images-eu.ssl-images-amazon.com/images/G/31/img2020/img21/apparelGW/augatf23/marmfd/new/WA_ETH_2x._CB580132721_.jpg"
         
     },
     {
@@ -12,7 +12,7 @@ export const CrouselData=[
         
     },
     {
-        image:"https://images-eu.ssl-images-amazon.com/images/G/31/IMG24/Smart_Watches/1500X600_Smartwatch_accuracy._CB581868877_.jpg"
+        image:"https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg"
         
     }
 
