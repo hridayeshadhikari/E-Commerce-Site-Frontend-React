59820b11d05678eb6fa2542d0e62f5fcb93dab86
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-03-15 23:30:43 +0530

    c1

--- e-commerce/src/User/Component/Order/Order.jsx
+++ e-commerce/src/User/Component/Order/Order.jsx
@@ -6,45 +6,50 @@ import { useDispatch, useSelector } from 'react-redux';
 import { orderHistory } from '../../../Redux/Order/Action';
 
 const Order = () => {
-    const navigate=useNavigate();
-    const dispatch=useDispatch();
-    const {order}=useSelector(store=>store);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { order } = useSelector(store => store);
     
-    
-    useEffect(()=>{
-        dispatch(orderHistory())
-    },[])
-    console.log("-----",order)
+    useEffect(() => {
+        dispatch(orderHistory());
+    }, []);
+
+    console.log("-----", order);
+
     return (
-       <div>
-        {
-         <div onClick={()=>navigate(`/account/order/${order.orders.id}`)} className="cursor-pointer container mt-8 lg:mx-auto mb-3 p-4 border shadow-md rounded-e-md justify-center">
-            <Grid container spacing={2} sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                <Grid item xs={12} sm={6}>
-                    <div className='flex'>
-                        <img src="https://image.made-in-china.com/2f0j00ceFlBLZnwzrO/New-Style-Casual-Men-Light-Running-Air-Cushion-Shoes-Hot-Selling-Shooes.webp" alt="Product" className="w-[8rem] h-auto" />
-                        <div className='flex flex-col p-2'>
-                            <p className='mt-3 font-semibold'>Mens Shooes</p>
-                            <p className='mt-2 font-semibold text-gray-500'>Size M</p>
-                            <p className='font-semibold text-gray-500'>color : Black</p>
-                        </div>
-                    </div>
-                </Grid>
+        <div>
+            {order.orders.map(orderItem => (
+                <div key={orderItem.id} onClick={() => navigate(`/account/order/${orderItem.id}`)} className="cursor-pointer container mt-8 lg:mx-auto mb-3 p-4 border shadow-md rounded-e-md justify-center">
+                    <Grid container spacing={2} sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                        {orderItem.orderItems.map(item => (
+                            <React.Fragment key={item.productId}>
+                                <Grid item xs={12} sm={6}>
+                                    <div className='flex'>
+                                        <img src={item.product.imageUrl} alt="Product" className="w-[8rem] h-auto" />
+                                        <div className='flex flex-col p-2'>
+                                            <p className='mt-3 font-semibold'>{item.product.title}</p>
+                                            <p className='mt-2 font-semibold text-gray-500'>Size {item.size}</p>
+                                            <p className='font-semibold text-gray-500'>Color: {item.product.color}</p>
+                                        </div>
+                                    </div>
+                                </Grid>
 
-                <Grid item xs={12} sm={3} className='font-semibold'>
-                    <p>Price ₹{order?.orders.discountPrice}</p>
-                </Grid>
-
-                <Grid item xs={12} sm={3}>
-                    <p className='font-semibold'>
-                        <AdjustIcon sx={{ color: "#66BB6A", marginRight: 1 }} />
-                        Expected Delhivery on March 15
-                        <p className='text-sm font-medium'>Your item is on the way</p>
-                    </p>
-                </Grid>
-            </Grid>
-        </div>}
-       </div>
+                                <Grid item xs={12} sm={3} className='font-semibold'>
+                                    <p>Price ₹{item.discountedPrice}</p>
+                                </Grid>
+                            </React.Fragment>
+                        ))}
+                        <Grid item xs={12} sm={3}>
+                            <p className='font-semibold'>
+                                <AdjustIcon sx={{ color: "#66BB6A", marginRight: 1 }} />
+                                Expected Delivery on March 15
+                                <p className='text-sm font-medium'>Your item is on the way</p>
+                            </p>
+                        </Grid>
+                    </Grid>
+                </div>
+            ))}
+        </div>
     );
 };
 
--- e-commerce/src/User/Component/Order/OrderDetail.jsx
+++ e-commerce/src/User/Component/Order/OrderDetail.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import OrderTracker from './OrderTracker'
 import AddressCard from '../AddressCard/AddressCard'
 import { Grid } from '@mui/material';
 import AdjustIcon from '@mui/icons-material/Adjust';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useParams } from 'react-router-dom';
+import { getOrderById } from '../../../Redux/Order/Action';
 
 const OrderDetail = () => {
+
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location);
+    const { order } = useSelector(store => store);
+    const { orderId } = useParams();
+
+    useEffect(() => {
+        dispatch(getOrderById(orderId));
+    }, [])
+
+    console.log("------order detatil-----", order)
+
     return (
         <div>
             <div className='ml-[4rem] mr-[4rem] mt-5'>
@@ -15,32 +31,34 @@ const OrderDetail = () => {
                 <OrderTracker />
             </div>
             <div>
-                <div className="container mt-8 lg:mx-auto mb-3 p-4 border shadow-md rounded-e-md  justify-center">
-                    <Grid container spacing={2} sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
-                        <Grid item xs={12} sm={6}>
-                            <div className='flex'>
-                                <img src="https://image.made-in-china.com/2f0j00ceFlBLZnwzrO/New-Style-Casual-Men-Light-Running-Air-Cushion-Shoes-Hot-Selling-Shooes.webp" alt="Product" className="w-[8rem] h-auto" />
-                                <div className='flex flex-col p-2'>
-                                    <p className='mt-3 font-semibold'>Mens Shooes</p>
-                                    <p className='mt-2 font-semibold text-gray-500'>Size M</p>
-                                    <p className='font-semibold text-gray-500'>color : Black</p>
+                {order.orderItems.map((item) => {
+                    <div className="container mt-8 lg:mx-auto mb-3 p-4 border shadow-md rounded-e-md  justify-center">
+                        <Grid container spacing={2} sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Grid item xs={12} sm={6}>
+                                <div className='flex'>
+                                    <img src="https://image.made-in-china.com/2f0j00ceFlBLZnwzrO/New-Style-Casual-Men-Light-Running-Air-Cushion-Shoes-Hot-Selling-Shooes.webp" alt="Product" className="w-[8rem] h-auto" />
+                                    <div className='flex flex-col p-2'>
+                                        <p className='mt-3 font-semibold'>Mens Shooes</p>
+                                        <p className='mt-2 font-semibold text-gray-500'>Size {item.size}</p>
+                                        <p className='font-semibold text-gray-500'>color : Black</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </Grid>
+                            </Grid>
 
-                        <Grid item xs={12} sm={3} className='font-semibold'>
-                            <p>Price ₹5999</p>
-                        </Grid>
+                            <Grid item xs={12} sm={3} className='font-semibold'>
+                                <p>Price ₹{order.order.discountPrice}</p>
+                            </Grid>
 
-                        <Grid item xs={12} sm={3}>
-                            <p className='font-semibold'>
-                                <AdjustIcon sx={{ color: "#66BB6A", marginRight: 1 }} />
-                                Expected Delhivery on March 15
-                                <p className='text-sm font-medium'>Your item is on the way</p>
-                            </p>
+                            <Grid item xs={12} sm={3}>
+                                <p className='font-semibold'>
+                                    <AdjustIcon sx={{ color: "#66BB6A", marginRight: 1 }} />
+                                    Expected Delhivery on March 15
+                                    <p className='text-sm font-medium'>Your item is on the way</p>
+                                </p>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </div>
+                    </div>
+                })}
             </div>
         </div>
     )
