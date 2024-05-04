3b0b24760be3a2ac02af620e99daeae41cd818eb
reetajoshi <reetajoshi949@gmail.com>
2024-05-04 21:23:14 +0530

    sign up , sign in page fixes

--- src/Auth/AuthModal.jsx
+++ src/Auth/AuthModal.jsx
@@ -1,46 +1,47 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import { useLocation } from 'react-router-dom';
-import Register from './Register';
-import Login from './Login';
+import React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import { useLocation } from "react-router-dom";
+import Register from "./Register";
+import Login from "./Login";
 
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 500,
-    height:600,
-    bgcolor: 'background.paper',
-    outline:'none',
-    boxShadow: 24,
-    p: 4,
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  height: 600,
+  bgcolor: "background.paper",
+  outline: "none",
+  boxShadow: 24,
+  p: 4,
 };
 
-const AuthModal = ({handleClose,open}) => {
-
-    const location=useLocation();
-    return (
-        <div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <Box sx={{
-                        ...style,
-                        '@media (max-width: 1024px)': {
-                            width: '90%',
-                            height:'100%', 
-                        },
-                    }}>
-                    {location.pathname==="/login"?<Login/>:<Register/>}
-                </Box>
-            </Modal>
-        </div>
-    )
-}
+const AuthModal = ({ handleClose, open }) => {
+  const location = useLocation();
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            ...style,
+            "@media (max-width: 1024px)": {
+              width: "90%",
+              height: "100%",
+            },
+          }}
+        >
+          {location.pathname === "/login" ? <Login /> : <Register />}
+        </Box>
+      </Modal>
+    </div>
+  );
+};
 
 export default AuthModal;
--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,164 +1,176 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 import sitelogo from "../Assets/trendsphere-high-resolution-logo-transparent.png";
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from '../Redux/Auth/Action';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../Redux/Auth/Action";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const defaultTheme = createTheme();
 
 // Define Yup schema for validation
 const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    password: Yup.string().required('Password is required'),
-    terms: Yup.boolean().oneOf([true], 'Must accept terms and conditions')
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string().required("Password is required"),
+  terms: Yup.boolean().oneOf([true], "Must accept terms and conditions"),
 });
 
 export default function SignUp() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const error = useSelector(state => state.auth.error);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const error = useSelector((state) => state.auth.error);
 
-    // Local state to control the visibility of the error message
-    const [showError, setShowError] = useState(false);
+  // Local state to control the visibility of the error message
+  const [showError, setShowError] = useState(false);
 
-    const handleSubmit = async (values, { setSubmitting }) => {
-        console.log("userdata", values);
-        setSubmitting(false);
-        dispatch(login(values));
-    };
+  const handleSubmit = async (values, { setSubmitting }) => {
+    console.log("userdata", values);
+    setSubmitting(false);
+    dispatch(login(values));
+  };
 
-    useEffect(() => {
-        if (error && showError) {
-            toast.error("Invalid email or password", {
-                position: "top-center",
-                autoClose: 2000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-            });
-        }
-    }, [error, showError]);
+  useEffect(() => {
+    if (error && showError) {
+      toast.error("Invalid email or password", {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }, [error, showError]);
 
-    return (
-        <div className='mb-[4rem]'>
-            <ThemeProvider theme={defaultTheme}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Box
-                        sx={{
-                            marginTop: 6,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <div className='mb-4'>
-                            <img className='h-[4rem] ' src={sitelogo} alt="" />
-                        </div>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                        </Typography>
-                        <Formik
-                            initialValues={{ email: '', password: '', terms: false }}
-                            validationSchema={validationSchema}
-                            onSubmit={(values, actions) => {
-                                setShowError(true); // Set showError to true on form submission
-                                handleSubmit(values, actions);
-                            }}
-                        >
-                            {({ isSubmitting, errors, touched }) => (
-                                <Form noValidate>
-                                    <Box component="div" sx={{ mt: 3 }}>
-                                        <Grid container spacing={2}>
-                                            <Grid item xs={12}>
-                                                <Field
-                                                    as={TextField}
-                                                    required
-                                                    fullWidth
-                                                    id="email"
-                                                    label="Email Address"
-                                                    name="email"
-                                                    autoComplete="email"
-                                                    error={errors.email && touched.email}
-                                                    helperText={errors.email && touched.email && errors.email}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <Field
-                                                    as={TextField}
-                                                    required
-                                                    fullWidth
-                                                    name="password"
-                                                    label="Password"
-                                                    type="password"
-                                                    id="password"
-                                                    autoComplete="new-password"
-                                                    error={errors.password && touched.password}
-                                                    helperText={errors.password && touched.password && errors.password}
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
+  return (
+    <div className="mb-[4rem]">
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 6,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <div className="mb-4">
+              <img className="h-[4rem] " src={sitelogo} alt="" />
+            </div>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Formik
+              initialValues={{ email: "", password: "", terms: false }}
+              validationSchema={validationSchema}
+              onSubmit={(values, actions) => {
+                setShowError(true); // Set showError to true on form submission
+                handleSubmit(values, actions);
+              }}
+            >
+              {({ isSubmitting, errors, touched }) => (
+                <Form noValidate>
+                  <Box component="div" sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <Field
+                          as={TextField}
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                          error={errors.email && touched.email}
+                          helperText={
+                            errors.email && touched.email && errors.email
+                          }
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <Field
+                          as={TextField}
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                          error={errors.password && touched.password}
+                          helperText={
+                            errors.password &&
+                            touched.password &&
+                            errors.password
+                          }
+                        />
+                      </Grid>
+                      {/* <Grid item xs={12}>
                                                 <FormControlLabel
                                                     control={<Field as={Checkbox} name="terms" color="primary" />}
                                                     label="Terms & conditions."
                                                     error={errors.terms && touched.terms}
                                                 />
                                                 {errors.terms && touched.terms && <div>{errors.terms}</div>}
-                                            </Grid>
-                                        </Grid>
-                                        <Button
-                                            type="submit"
-                                            fullWidth
-                                            variant="contained"
-                                            sx={{ mt: 3, mb: 2 }}
-                                            disabled={isSubmitting}
-                                        >
-                                            Sign in
-                                        </Button>
-                                        <ToastContainer
-                                            position="top-center"
-                                            autoClose={5000}
-                                            hideProgressBar={false}
-                                            newestOnTop={false}
-                                            closeOnClick
-                                            rtl={false}
-                                            pauseOnFocusLoss
-                                            draggable
-                                            pauseOnHover
-                                            theme="light"
-                                        />
-                                        <Grid container justifyContent="flex-end">
-                                            <Grid item>
-                                                <p className='text-sm text-blue-600'>
-                                                    Don't have an account? <button className="cursor-pointer hover:underline" onClick={() => navigate("/signup")}>Sign up</button>
-                                                </p>
-                                            </Grid>
-                                        </Grid>
-                                    </Box>
-                                </Form>
-                            )}
-                        </Formik>
-                    </Box>
-                </Container>
-            </ThemeProvider>
-        </div>
-    );
+                                            </Grid> */}
+                    </Grid>
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                      disabled={isSubmitting}
+                    >
+                      Sign in
+                    </Button>
+                    <ToastContainer
+                      position="top-center"
+                      autoClose={5000}
+                      hideProgressBar={false}
+                      newestOnTop={false}
+                      closeOnClick
+                      rtl={false}
+                      pauseOnFocusLoss
+                      draggable
+                      pauseOnHover
+                      theme="light"
+                    />
+                    <Grid container justifyContent="flex-end">
+                      <Grid item>
+                        <p className="text-sm text-black-600">
+                          Don't have an account ?{" "}
+                          <button
+                            className="cursor-pointer underline text-blue-600"
+                            onClick={() => navigate("/signup")}
+                          >
+                            Sign up
+                          </button>
+                        </p>
+                      </Grid>
+                    </Grid>
+                  </Box>
+                </Form>
+              )}
+            </Formik>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </div>
+  );
 }
